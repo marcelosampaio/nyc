6b7a03e5521709078aa074f5c35a3c339edf4869
Marcelo Sampaio <marcelo@artlogica.com.br>
2014-01-12 19:16:17 -0200

    Initial Commit

---
+++ nyc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  nyc
+//
+//  Created by Marcelo Sampaio on 1/12/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
