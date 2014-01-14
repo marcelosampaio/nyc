8411f189bd4f968857e0c03fdbb8817c630daca9
Marcelo Sampaio <marcelo@artlogica.com.br>
2014-01-14 18:49:50 -0200

    detail page implemented

---
+++ nyc/detailViewController.h
@@ -0,0 +1,13 @@
+//
+//  detailViewController.h
+//  nyc
+//
+//  Created by Marcelo Sampaio on 1/14/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface detailViewController : UIViewController
+
+@end
---
+++ nyc/detailViewController.m
@@ -0,0 +1,39 @@
+//
+//  detailViewController.m
+//  nyc
+//
+//  Created by Marcelo Sampaio on 1/14/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import "detailViewController.h"
+
+@interface detailViewController ()
+
+@end
+
+@implementation detailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    self.navigationItem.title=@"Detail Title";
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
